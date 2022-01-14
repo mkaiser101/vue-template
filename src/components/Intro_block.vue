d09ea21a301ae8a738ea83959c64130e815e3114
Matthew Kaiser <mk@Matthews-MacBook-Air.local>
2022-01-14 10:45:06 -0800

    added branch

---
+++ src/components/Intro_block.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app>
+   <v-carousel>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+    </v-app>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: require('../assets/logo.png')
+          },
+          {
+            src: require('../assets/distortedtree.jpg')
+          },
+          {
+            src: require('../assets/logo.png')
+          },
+          {
+            src: require('../assets/distortedtree.jpg')
+          }
+        ]
+      }
+    }
+  }
+</script>
